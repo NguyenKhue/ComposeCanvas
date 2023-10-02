f4657a6f6d293d65118e9182b306f77d4f9b344d
NguyenKhue <nguyenkhue09032001@gmail.com>
2023-10-03 02:02:39 +0700

    Create chart

---
+++ app/src/main/java/com/khue/composecanvas/MainActivity.kt
@@ -0,0 +1,129 @@
+package com.khue.composecanvas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawOutline
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.khue.composecanvas.ui.theme.ComposeCanvasTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalTextApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeCanvasTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.onSecondaryContainer
+                ) {
+                    Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                        val x = ((16*150) + 100).pxToDp()
+                        StockChart(
+                            modifier = Modifier
+                                .horizontalScroll(rememberScrollState())
+                                .width(x)
+                                .height(300.dp)
+                                .align(CenterHorizontally),
+                            infos = listOf(249.75f to 4, 250.35f to 5, 249.81f to 6, 249.17f to 7, 250.61f to 8, 253.24f to 9, 252.9f to 10, 252.0f to 11,
+                                250.856f to 12, 246.77f to 13, 250.39f to 14, 250.23f to 15, 250.01f to 16 , 249.97f to 17 , 249.89f to 18, 249.95f to 19),
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Int.pxToDp() = with(LocalDensity.current) { this@pxToDp.toDp() }
+
+//[IntradayInfo(date=2023-09-29T04:00, close=249.75),
+//IntradayInfo(date=2023-09-29T05:00, close=250.35),
+//IntradayInfo(date=2023-09-29T06:00, close=249.81),
+//IntradayInfo(date=2023-09-29T07:00, close=249.17),
+//IntradayInfo(date=2023-09-29T08:00, close=250.61),
+//IntradayInfo(date=2023-09-29T09:00, close=253.24),
+//IntradayInfo(date=2023-09-29T10:00, close=252.9),
+//IntradayInfo(date=2023-09-29T11:00, close=252.0),
+//IntradayInfo(date=2023-09-29T12:00, close=250.856),
+//IntradayInfo(date=2023-09-29T13:00, close=246.77),
+//IntradayInfo(date=2023-09-29T14:00, close=250.39),
+//IntradayInfo(date=2023-09-29T15:00, close=250.23),
+//IntradayInfo(date=2023-09-29T16:00, close=250.01),
+//IntradayInfo(date=2023-09-29T17:00, close=249.97),
+//IntradayInfo(date=2023-09-29T18:00, close=249.89),
+//IntradayInfo(date=2023-09-29T19:00, close=249.95)]
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+
+        // Creating a canvas and creating a triangular path
+        Canvas(modifier = Modifier.fillMaxWidth().aspectRatio(1f)) {
+
+            val rect = Rect(Offset.Zero, size)
+            val trianglePath = Path().apply {
+                moveTo(rect.topCenter.x, rect.topCenter.y)
+                lineTo(rect.bottomRight.x, rect.bottomRight.y)
+                lineTo(rect.bottomLeft.x, rect.bottomLeft.y)
+                lineTo(500f, 150f)
+                close()
+            }
+
+            drawPath(
+                trianglePath,
+                color = Color.Green,
+            )
+//            // Adding a path effect of rounded corners
+//            drawIntoCanvas { canvas ->
+//                canvas.drawOutline(
+//                    outline = Outline.Generic(trianglePath),
+//                    paint = Paint().apply {
+//                        color = Color.Green
+//                        pathEffect = PathEffect.cornerPathEffect(rect.maxDimension / 3)
+//                    }
+//                )
+//            }
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeCanvasTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/khue/composecanvas/StockChart.kt
@@ -0,0 +1,136 @@
+package com.khue.composecanvas
+
+import android.graphics.Paint
+import android.util.Log
+import androidx.compose.foundation.Canvas
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.asAndroidPath
+import androidx.compose.ui.graphics.asComposePath
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlin.math.round
+import kotlin.math.roundToInt
+
+
+@Composable
+@ExperimentalTextApi
+fun StockChart(
+    modifier: Modifier = Modifier,
+    infos: List<Pair<Float, Int>> = emptyList(),
+    graphColor: Color = Color.Green
+) {
+    val spacing = 100f
+    val transparentGraphColor = remember {
+        graphColor.copy(alpha = 0.5f)
+    }
+    val upperValue = remember(infos) {
+        (infos.maxOfOrNull { it.first }?.plus(1))?.roundToInt() ?: 0
+    }
+    val lowerValue = remember(infos) {
+        infos.minOfOrNull { it.first }?.toInt() ?: 0
+    }
+
+    val textMeasure = rememberTextMeasurer()
+
+    Canvas(
+        modifier = modifier,
+    ) {
+        val spacePerHour = 150f
+        infos.indices.forEach { i ->
+            val info = infos[i]
+            val hour = info.second
+
+            val hourTextMeasure = textMeasure.measure(
+                text = hour.toString(),
+                style = TextStyle(
+                    fontSize = 12.sp,
+                    color = Color.White,
+                    textAlign = TextAlign.Center
+                ),
+            )
+            Log.d("StockChart", "StockChart: $i")
+            drawText(hourTextMeasure, topLeft = Offset(spacing + i * spacePerHour, size.height - 5))
+        }
+
+        val priceStep = (upperValue - lowerValue) / 5f
+        (0..4).forEach { i ->
+            val priceTextMeasure = textMeasure.measure(
+                text = round(lowerValue + priceStep * i).toString(),
+                style = TextStyle(
+                    fontSize = 12.sp,
+                    color = Color.White,
+                    textAlign = TextAlign.Center
+                ),
+            )
+            drawText(priceTextMeasure, topLeft = Offset(20f, size.height - spacing - i * size.height / 5f))
+        }
+
+        var lastX = 0f
+
+        val strokePath = Path().apply {
+            val height = size.height
+            for (i in infos.indices) {
+                val info = infos[i]
+                val nextInfo = infos.getOrNull(i + 1) ?: infos.last()
+                val leftRatio = (info.first - lowerValue) / (upperValue - lowerValue)
+                val rightRatio = (nextInfo.first - lowerValue) / (upperValue - lowerValue)
+
+                val x1 = spacing + i * spacePerHour
+                val y1 = height - spacing - (leftRatio * height)
+                val x2 = spacing + (i + 1) * spacePerHour
+                val y2 = height - spacing - (rightRatio * height)
+                if (i == 0) {
+                    moveTo(x1, y1)
+                }
+
+                lastX = (x1 + x2) / 2f
+
+                quadraticBezierTo(
+                    x1, y1, lastX, (y1 + y2) / 2f
+                )
+            }
+        }
+
+        val fillPath = android.graphics.Path(strokePath.asAndroidPath())
+            .asComposePath()
+            .apply {
+                lineTo(lastX, size.height - spacing)
+                lineTo(spacing, size.height - spacing)
+                close()
+            }
+
+        drawPath(
+            path = fillPath,
+            brush = Brush.verticalGradient(
+                colors = listOf(
+                    transparentGraphColor,
+                    Color.Transparent
+                ),
+                endY = size.height - spacing
+            )
+        )
+        drawPath(
+            path = strokePath,
+            color = graphColor,
+            style = Stroke(
+                width = 3.dp.toPx(),
+                cap = StrokeCap.Round
+            )
+        )
+    }
+}
